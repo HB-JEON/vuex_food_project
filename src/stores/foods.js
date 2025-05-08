d09d5e3ab43465767b08f69094d20ee260e2c587
HB-JEON <wjsgusqls@nate.com>
2025-05-08 16:57:53 +0900

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.vue 	deleted:    src/components/HelloWorld.vue 	modified:   src/main.js 	new file:   src/router/index.js 	new file:   src/stores/foods.js 	new file:   src/views/FoodDetailView.vue 	new file:   src/views/FoodListView.vue

---
+++ src/stores/foods.js
@@ -0,0 +1,38 @@
+import { defineStore } from "pinia";
+import axios from "axios";
+
+export const useFoodStore = defineStore('foods', {
+    state: () => ({
+        food_data: {},
+        food_detail: {}
+    }),
+    actions: {
+        foodListData(page) {
+            axios.get('http://127.0.0.1:8000/web/food_list/', {
+                params: {
+                    page: page
+                }
+            }).then(res => {
+                this.food_data = res.data
+            })
+        },
+        range(start, end) {
+            let arr = []
+            let len = end - start + 1
+            for(let i=0; i<len; i++) {
+                arr[i] = start
+                start++
+            }
+            return arr
+        },
+        foodDetailData(fno) {
+            axios.get('http://127.0.0.1:8000/web/food_detail/', {
+                params: {
+                    fno: fno
+                }
+            }).then(res => {
+                this.food_detail = res.data
+            })
+        }
+    } 
+})
---
+++ src/views/FoodDetailView.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <table class="table">
+          <tbody>
+          <tr>
+            <td width="30%" class="text-center" rowspan="8">
+              <img :src="'https://www.menupan.com'+foods.food_detail.poster" style="width: 240px;height: 280px;">
+            </td>
+            <td colspan="2">
+                <h3>{{ foods.food_detail.name }}&nbsp;
+                    <span style="color: orange;">{{ foods.food_detail.score }}</span>
+                </h3>
+            </td>
+          </tr>
+          <tr>
+            <th width="15%">주소</th>
+            <td width="55%">{{ foods.food_detail.address }}</td>
+          </tr>
+          <tr>
+            <th width="15%">전화</th>
+            <td width="55%">{{ foods.food_detail.phone }}</td>
+          </tr>
+          <tr>
+            <th width="15%">음식 종류</th>
+            <td width="55%">{{ foods.food_detail.type }}</td>
+          </tr>
+          <tr>
+            <th width="15%">주차</th>
+            <td width="55%">{{ foods.food_detail.parking }}</td>
+          </tr>
+          <tr>
+            <th width="15%">영업 시간</th>
+            <td width="55%">{{ foods.food_detail.time }}</td>
+          </tr>
+          <tr>
+            <th width="15%">테마</th>
+            <td width="55%">{{ foods.food_detail.theme }}</td>
+          </tr>
+         </tbody>
+        </table>
+        <table class="table">
+          <tr>
+            <td>{{ foods.food_detail.content }}</td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { onMounted } from 'vue'
+import { useFoodStore } from '@/stores/foods'
+import { useRoute } from 'vue-router'
+    
+    const route = useRoute()
+    const foods = useFoodStore()
+    onMounted(() => {
+      foods.foodDetailData(route.params.fno)
+    })
+</script>
+<style>
+.container {
+    margin-top: 50px;
+}
+.row {
+    margin: 0px auto;
+    width: 800px;
+}
+</style>
